0fc20ff8e77b6747735f52ddf441e9452b149407
joeltlc96 <145817552+joeltlc96@users.noreply.github.com>
2025-06-22 21:42:13 -0500

    Fix routes and add missing dependency

--- src/routes/pregunta.route.js
+++ src/routes/pregunta.route.js
@@ -1,10 +1,8 @@
 import { Router } from 'express'
-import { generarPregunta, enviarRespuesta, mostrarHistorial } from '../controllers/pregunta.controller.js'
+import { generarPregunta } from '../controllers/pregunta.controller.js'
 
 const router = Router()
 
 router.post('/pregunta', generarPregunta)
-router.post('/feedback', enviarRespuesta)
-router.get('/historial', mostrarHistorial)
 
 export default router
